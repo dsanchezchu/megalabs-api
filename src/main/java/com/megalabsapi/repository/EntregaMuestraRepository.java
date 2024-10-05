f313044b7a494f938cab11ad6f3ff916134682db
Diego Sanchez Chuquimango <diego2702015@outlook.com>
2024-10-04 23:58:57 -0500

    feat(Backend) : filtro de fecha entrega venta y swagger (implementado)

--- src/main/java/com/megalabsapi/repository/EntregaMuestraRepository.java
+++ src/main/java/com/megalabsapi/repository/EntregaMuestraRepository.java
@@ -3,8 +3,11 @@ package com.megalabsapi.repository;
 import com.megalabsapi.model.entity.Entrega_Muestra;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface EntregaMuestraRepository extends JpaRepository<Entrega_Muestra,Integer> {
     //Logica de acceso a base de datos para ejecutar las queries
     //Heredar el CRUD
-
+    List<Entrega_Muestra> findByFecha(LocalDateTime fecha);
 }
