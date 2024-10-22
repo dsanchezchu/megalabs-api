7cd338d254d7ddcbf21c052f934feb4dc0145cdf
alexandercordovaplasencia <alexandercordovaplasencia@gmail.com>
2024-10-22 13:21:26 -0500

    feat(backend):desarrolloreporteventas

---
+++ src/main/java/com/megalabsapi/repository/DetalleVentaRepository.java
@@ -0,0 +1,15 @@
+package com.megalabsapi.repository;
+
+import com.megalabsapi.entity.Detalle_Venta;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface DetalleVentaRepository extends JpaRepository<Detalle_Venta, Integer> {
+
+    @Query("SELECT d.producto.idProducto, SUM(d.cantidad) as totalVentas FROM Detalle_Venta d GROUP BY d.producto.idProducto")
+    List<Object[]> obtenerReporteVentas();
+}
