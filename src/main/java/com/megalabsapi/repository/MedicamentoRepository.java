e61ceeeba02ce2dd56dc511185d717331b7de474
alexandercordovaplasencia <alexandercordovaplasencia@gmail.com>
2024-10-22 11:08:38 -0500

    feat(backend): correcciones para realizar la pull request a develop

---
+++ src/main/java/com/megalabsapi/repository/MedicamentoRepository.java
@@ -0,0 +1,9 @@
+package com.megalabsapi.repository;
+
+import com.megalabsapi.entity.Medicamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface MedicamentoRepository extends JpaRepository<Medicamento, Long> {
+    List<Medicamento> findByCantidadGreaterThan(int cantidad);
+}
